5b0cfa5449c9c873762c9455ba0d9c0f5fc756f5
Reagan Bodily <reaganbodily@gmail.com>
2022-04-12 09:29:47 -0600

    building tables

--- db/schema.sql
+++ db/schema.sql
@@ -24,5 +24,6 @@ CREATE TABLE employee(
     last_name VARCHAR(30),
     role_id INTEGER,
     manager_id INTEGER
-    CONSTRAINT 
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    
 )
